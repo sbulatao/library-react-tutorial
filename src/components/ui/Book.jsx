06b17834f73b64140afc4d2c73230a5ce167bc4c
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-03-27 00:58:59 -0700

    Featured Books

---
+++ src/components/ui/Book.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Book({ book }) {
+  return (
+     <div className="book">
+        <a href="">
+            <figure className="book__img--wrapper">
+                <img src={book.url}
+                alt="" 
+                className="book__img" 
+                />
+            </figure>
+        </a>
+        <div className="book__title">
+            <a href="/" className='book__title--link'>
+                {book.title}
+            </a>
+        </div>
+        <div className="book__ratings">
+            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
+            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
+            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
+            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
+            <FontAwesomeIcon icon="star-half-alt"></FontAwesomeIcon>
+        </div>
+
+        <div className="book__price">
+            <span className="book__price--normal">${book.originalPrice}</span>
+            ${book.salePrice}
+        </div>
+    </div>
+  )
+}
