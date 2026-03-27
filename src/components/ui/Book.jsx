0a342e89277d59fa6762e995e591a8438045af55
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-03-27 01:17:13 -0700

    Features: Working on Stars Rating part 1

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Book({ book }) {
+    const rating = 4;
+
+    // <FontAwesomeIcon icon="star" />
+    // <FontAwesomeIcon icon="star" />
+    // <FontAwesomeIcon icon="star" />
+    // <FontAwesomeIcon icon="star" />
+
   return (
      <div className="book">
         <a href="">
@@ -18,11 +25,11 @@ export default function Book({ book }) {
             </a>
         </div>
         <div className="book__ratings">
-            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
-            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
-            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
-            <FontAwesomeIcon icon="star"></FontAwesomeIcon>
-            <FontAwesomeIcon icon="star-half-alt"></FontAwesomeIcon>
+            {   // RATING HERE
+                new Array(5).fill(0).map((_, index) =>
+                    <FontAwesomeIcon icon="star" key={index} />
+                )
+            }
         </div>
 
         <div className="book__price">
