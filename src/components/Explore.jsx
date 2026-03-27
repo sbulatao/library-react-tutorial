23f1d689d5ade976a35df852445ad2e17809da7f
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-03-27 01:44:59 -0700

    Added Explore section

---
+++ src/components/Explore.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Explore() {
+  return (
+    <section id="explore">
+        <div className="container">
+            <div className="row row__column">
+                <h2>
+                    Explore More <span className="purple">Books</span>
+                </h2>
+                <a href="/books">
+                <button className="btn">Explore Books</button></a>
+            </div>
+        </div>
+    </section>
+  )
+}
