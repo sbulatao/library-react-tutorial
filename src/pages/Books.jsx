8025509e7916491162640f6105958b1f106f0810
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-03-27 03:17:39 -0700

    Added Filtering on /books

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { books } from '../data' 
 import Book from '../components/ui/Book'
 
-export default function Books() {
+const Books = ({ books: initialBooks}) => {
+    const [books, setBooks] = useState(initialBooks);
+
+    function filterBooks(filter){
+        console.log(filter)
+        if(filter === 'LOW_TO_HIGH'){
+            // take a.salePrice, if a.salePrice doesnt exist, use the a.originalPrice - take b.salePrice, if b.salePrice doesnt exist, use the b.originalPrice
+            // books.sort((a,b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice))
+            //.slice -> take a clone
+            setBooks(books.slice().sort((a,b) => 
+                (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))
+        }
+        if(filter === 'HIGH_TO_LOW'){
+            // take a.salePrice, if a.salePrice doesnt exist, use the a.originalPrice - take b.salePrice, if b.salePrice doesnt exist, use the b.originalPrice
+            // books.sort((b,a) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice))
+            //.slice -> a clone
+            setBooks(books.slice().sort((b,a) => 
+                (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))
+        }
+        if(filter === 'RATING'){
+            // take a.rating, if a.rating doesnt exist, use the a.rating - take b.rating, if b.rating doesnt exist, use the b.rating
+            // books.sort((b,a) => (a.rating || a.rating) - (b.rating || b.rating))
+            //.slice -> a clone
+            setBooks(books.slice().sort((b,a) =>
+                 (a.rating || a.rating) - (b.rating || b.rating)))
+        }
+        
+    }
+
   return (
     <div id="books__body">
         <main id="books__main">
@@ -10,8 +38,10 @@ export default function Books() {
                 <div className="books__container">
                     <div className="row">
                         <div className="books__header">
-                            <h2 className="section__title books__header--title">All Books</h2>
-                            <select name="" id="filter" defaultValue="DEFAULT">
+                            <h2 className="section__title books__header--title">
+                                All Books
+                            </h2>
+                            <select name="" id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
                                 <option value="DEFAULT" disabled>Sort</option>
                                 <option value="LOW_TO_HIGH">Price, Low to High </option>
                                 <option value="HIGH_TO_LOW">Price, High to Low</option>
@@ -31,5 +61,7 @@ export default function Books() {
         </main>
       
     </div>
-  )
+  );
 }
+
+export default Books;
