cfb72470801fe0074a359f2c230175498e733e2b
sbulatao <114209345+sbulatao@users.noreply.github.com>
2026-03-30 02:12:40 -0700

    Still adding cart checking

--- src/App.js
+++ src/App.js
@@ -14,14 +14,22 @@ function App() {
   function addToCart(book){
     const dupeItem = cart.find(item => +item.id === +book.id)
     if (dupeItem){
-      dupeItem.quantity += 1;
+      // dupeItem.quantity += 1;
+      setCart(cart.map(item => {
+        if (item.id === dupeItem.id){
+          return{
+            ...item,
+            quantity: item.quantity + 1,
+          }
+        }
+      }))
     }
-    console.log(dupeItem)
+    // console.log(dupeItem)
     setCart([...cart, {...book, quantity: 1}])
   }
 
   useEffect(() => {
-    // console.log(cart);
+    console.log(cart);
   }, [cart])
 
   return (
